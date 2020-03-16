b7d6fb4e42e2c6843a1c6f29048d71f5859dc305
Berislav <berislav.hunjadi@gmail.com>
2020-03-16 08:47:02 +0100

    Initial commit

---
+++ Main.jsx
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+import {sum} from "/utils";
+
+export const Mail = props => {
+    // 1. uncomment useState line
+    // 2. position cursor on useState (at the end) and trigger completion (Ctrl + Space)
+    // 3. useState is added to already existing "import React from 'react'" which is correct
+    // const [value, setValue] = useState();
+    return <div>{sum(1, 4)}</div>;
+};
---
+++ Main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {sum} from "/utils";
+
+export const Mail = props => {
+    // 1. uncomment useState line
+    // 2. position cursor on useState (at the end) and trigger completion (Ctrl + Space)
+    // 3. import clause "import {useState} from "/node_modules/@types/react";" is added
+    // const [value, setValue] = useState();
+    return <div>{sum(1, 4)}</div>;
+};
